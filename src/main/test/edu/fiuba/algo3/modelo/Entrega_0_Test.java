bf26d7983c3cbe6656cf4b82acf286db72d34d60
Mariano Baffetti <marianobaffetti@gmail.com>
2020-07-23 23:16:56 -0300

    Se renombra test de entrega 0.

--- src/main/test/edu/fiuba/algo3/modelo/Entrega_0_Test.java
+++ src/main/test/edu/fiuba/algo3/modelo/Entrega_0_Test.java
@@ -7,15 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Entrega_0_Test {
     @Test
-    public void VerdaderoFalsoClasicoXXXXXXXXXXXXXXXXXXXXXXXXXX() {
-    // Una Pregunta de Verdadero/Falso clásico puede crearse indicándole cual es la respuesta
-    // correcta
+    public void PreguntaVerdaderoFalsoClasicoSeCreaYSeVerificaRespuesta() {
+        // Una Pregunta de Verdadero/Falso clásico puede crearse indicándole cual es la respuesta
+        // correcta
 
+        // Respuesta correcta
         var pregunta = new VerdaderoFalso("Colón llegó a América en el siglo XV.", true);
         var respuestaCorrecta = true;
         var resultado = pregunta.obtenerResultado(respuestaCorrecta);
 
         Assertions.assertTrue(resultado.esCorrecto());
+
+        // Respuesta incorrecta
 
         pregunta = new VerdaderoFalso("Colón llegó a América en el siglo XV.", false);
         respuestaCorrecta = false;
@@ -24,16 +27,16 @@ public class Entrega_0_Test {
         Assertions.assertFalse(resultado.esCorrecto());
     }
 
-//    @Test
-//    public void VerdaderoFalsoClasicoXXXXXXXXXXXXXXXXXXXXXXXXXX() {
-////        Una Pregunta de Verdadero/Falso clásico recibe una lista de respuestas y asigna
-////        correctamente puntos a los jugadores que respondieron correctamente
-//
-//        var pregunta = new VerdaderoFalso("Colón llegó a América en el siglo XV.", true);
-//        var respuestaCorrecta = true;
-//        var resultado = pregunta.obtenerResultado(respuestaCorrecta);
-//
-//        Assertions.assertTrue(resultado.esCorrecto());
-//    }
+    @Test
+    public void PreguntaVerdaderoFalsoClasicoRecibeMultiplesRespuestasYAsignaPuntosAJugadores() {
+        // Una Pregunta de Verdadero/Falso clásico recibe una lista de respuestas y asigna
+        // correctamente puntos a los jugadores que respondieron correctamente
+
+        var pregunta = new VerdaderoFalso("Colón llegó a América en el siglo XV.", true);
+        var respuestaCorrecta = true;
+        var resultado = pregunta.obtenerResultado(respuestaCorrecta);
+
+        Assertions.assertTrue(resultado.esCorrecto());
+    }
 
 }
